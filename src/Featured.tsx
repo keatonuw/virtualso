4c394c707a811fcd77af5d92ff2691a4714fd2d4
Hongyi <jihongyipinghe@gmail.com>
2021-10-24 00:58:35 -0700

    whatever

--- src/Featured.tsx
+++ src/Featured.tsx
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class Featured extends Component<{}, {}> {
     render() {
         return (
-            <div >
-                <h1>Featured Artist</h1>
-            </div>
+            <div></div>
         )
     }
 }
