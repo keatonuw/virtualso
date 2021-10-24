35bd628e56a2abb374f3eedd5672ee88bc438761
Keaton Kowal <kowalk@uw.edu>
2021-10-24 00:56:54 -0700

    carousel

--- src/Featured.tsx
+++ src/Featured.tsx
@@ -3,9 +3,32 @@ import React, { Component } from 'react';
 class Featured extends Component<{}, {}> {
     render() {
         return (
-            <div >
-                <h1>Featured Artist</h1>
-            </div>
+            <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                    <img src="https://assets.digitalocean.com/articles/alligator/css/object-fit/example-object-fit.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                    <img src="https://assets.digitalocean.com/articles/alligator/css/object-fit/example-object-fit.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                    <img src="https://assets.digitalocean.com/articles/alligator/css/object-fit/example-object-fit.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+                </div>
         )
     }
 }
